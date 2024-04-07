dc916f08d84cf45ff46f3064063891c47306e3d8
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-07 16:10:27 +0500

    add Content

---
+++ Models/Content.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GetOut.Models;
+
+public class Content
+{
+    public readonly Dictionary<string, Texture2D> Textures = new();
+
+    public Content(ContentManager content)
+    {
+        Textures.Add("wall", content.Load<Texture2D>("walls"));
+    }
+}
