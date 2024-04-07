15464ccabe54d845c532027babab7492d15cd8c1
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-07 16:11:04 +0500

    changes in Game1 for tests

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.IO;
+using System.Linq;
+using GetOut.Models;
+using GetOut.Services;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,10 +13,21 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Content _content;
+    private string[] _namesMazes;
+    private string[,] _currentMaze;
 
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
+        _graphics.PreferredBackBufferHeight = 1080;
+        _graphics.PreferredBackBufferWidth = 1920;
+        _graphics.IsFullScreen = true;
+        // _graphics.PreferMultiSampling = true; // Сглажение краёв
+        _graphics.ApplyChanges();
+        // Window.IsBorderless = true;
+        // Window.AllowUserResizing = true;
+
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
@@ -19,14 +35,15 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-
+        // GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        _content = new Content(Content); //
+        _namesMazes = MazeLoader.LoadNamesFromFolder(new DirectoryInfo("../../../Mazes")).ToArray(); //
         // TODO: use this.Content to load your game content here
     }
 
@@ -38,12 +55,21 @@ public class Game1 : Game
 
         // TODO: Add your update logic here
 
+        _currentMaze =
+            MazeToGraphic.ConvertMazeToGraphic(MazeLoader.LoadMazeFromFolder(new DirectoryInfo("../../../Mazes"),
+                _namesMazes.First())); //
+        View.Draw.DrawMaze(_graphics, _spriteBatch, _currentMaze, _content); // ТЕСТ
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        View.Draw.DrawMaze(_graphics,
+            _spriteBatch,
+            _currentMaze,
+            _content);
 
         // TODO: Add your drawing code here
 
