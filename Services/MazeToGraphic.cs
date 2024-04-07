645bf5158167423a39a28e7283744b8753c01b22
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-07 16:10:38 +0500

    add MazeTographic

---
+++ Services/MazeToGraphic.cs
@@ -0,0 +1,23 @@
+﻿using GetOut.Models;
+
+namespace GetOut.Services;
+
+public class MazeToGraphic
+{
+    public static string[,] ConvertMazeToGraphic(string[][] maze)
+    {
+        var height = maze.Length;
+        var width = maze[0].Length;
+        var graphicMaze = new string[width, height];
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                graphicMaze[x, y] = Wall.ConvertToGraphic(maze[y][x]);
+                // graphicMaze[x, y] = maze[y][x];
+            }
+        }
+
+        return graphicMaze;
+    }
+}
