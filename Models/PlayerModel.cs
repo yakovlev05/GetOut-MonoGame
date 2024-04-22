cf47b64205034f2283cba8558edad084e6f479dc
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-22 19:49:36 +0500

    add PlayerModel

---
+++ Models/PlayerModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GetOut.Models;
+
+public class PlayerModel
+{
+    public Texture2D Texture { get; init; }
+    public Vector2 Position { get; set; }
+    public Rectangle Rectangle { get; init; }
+
+    public PlayerModel(Texture2D texture, Vector2 position, Rectangle rectangle)
+    {
+        Texture = texture;
+        Position = position;
+        Rectangle = rectangle;
+    }
+}
