7424c20f299c19243accd4a4d2a643de2f9b27bb
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2024-04-22 17:33:51 +0500

    add MazeCellModel

---
+++ Models/MazeCellModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GetOut.Models;
+
+public class MazeCellModel
+{
+    public int TileX { get; init; }
+    public int TileY { get; init; }
+    public int Width { get; init; }
+    public int Height { get; init; }
+    public Texture2D Texture { get; init; }
+    public Rectangle Rectangle { get; init; }
+
+    public MazeCellModel(Texture2D texture, int tileX, int tileY, int width, int height)
+    {
+        TileX = tileX;
+        TileY = tileY;
+        Width = width;
+        Height = height;
+        Texture = texture;
+        Rectangle = new Rectangle(new Point(TileX, TileY), new Point(width, height));
+    }
+}
