b6e6eb49ecb1658433f59da6aa045c0fbc185d3c
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-05-01 12:53:21 +0500

    fix  packages

--- Game1.cs
+++ Game1.cs
@@ -28,8 +28,8 @@ public class Game1 : Game
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
-        // _graphics.PreferredBackBufferHeight = 1080;
-        // _graphics.PreferredBackBufferWidth = 1920;
+        _graphics.PreferredBackBufferHeight = 1080;
+        _graphics.PreferredBackBufferWidth = 1920;
         _graphics.IsFullScreen = true;
         // _graphics.PreferMultiSampling = true; // Сглажение краёв
         _graphics.ApplyChanges();
