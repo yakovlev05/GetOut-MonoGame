66d4d4e21f2e46fa0c8b2aef8e917dd408a4eee4
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-05-16 18:00:37 +0500

    add GameOverScreen graphic

---
+++ Content/GameOverScreen/menu_button_active.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="menu_button_active" tilewidth="16" tileheight="16" tilecount="200" columns="20">
+ <image source="menu_button_active.png" width="320" height="160"/>
+</tileset>
