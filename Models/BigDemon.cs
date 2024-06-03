206518f52e5ec1458a2aefc1c7af5df549f0ab2c
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-06-03 21:58:42 +0500

    add abstract class StupidMonster, update BigDemon

--- Models/BigDemon.cs
+++ Models/BigDemon.cs
@@ -1,106 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using GetOut.Controllers;
 using GetOut.Program;
 using GetOut.View;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGame.Extended;
 
 namespace GetOut.Models;
 
-public class BigDemon : IEntityInterface
+public class BigDemon : StupidMonster
 {
-    public MapController MapController { get; set; }
-    public Hero Hero { get; set; }
-    public Hearts Hearts { get; set; }
+    public sealed override Hearts Hearts { get; set; }
+    public sealed override AnimationController Anims { get; init; } = new();
 
-    private AnimationController Anims { get; init; } = new();
-
-    private Vector2 PositionInWorld { get; set; }
-    private float Speed { get; set; }
-    public int Width => 32;
-    public int Height => 36;
-
-    private List<Tuple<Point, int>> PathInWorldPoints { get; set; }
-    private int CurrentPointPathIndex { get; set; } = 0;
-    private float WaitTime { get; set; }
+    public override int Width => 32;
+    public override int Height => 36;
 
     public BigDemon(Vector2 positionInWorld, List<Tuple<Point, int>> pathInWorldPoints, int heartsCount = 3,
-        float speed = 1)
+        float speed = 1) : base(positionInWorld, pathInWorldPoints, speed)
     {
-        PositionInWorld = positionInWorld;
         Hearts = new Hearts(PositionInWorld, heartsCount, 0.35f, new Vector2(8, 0), 3);
-        Speed = speed;
-        PathInWorldPoints = pathInWorldPoints;
-        WaitTime = PathInWorldPoints.First().Item2;
 
         var texture = Globals.Content.Load<Texture2D>("./Levels/assets/BigDemon");
 
         Anims.AddAnimation("idle", new Animation(texture, 4, 2, 0.1f, 2, 4));
         Anims.AddAnimation("run_right", new Animation(texture, 4, 2, 0.1f, 1, 4));
         Anims.AddAnimation("run_left", new Animation(texture, 4, 2, 0.1f, 1, 4, true));
-    }
-
-    public void Update()
-    {
-        if (Hearts.IsDied) return;
-
-        if (IsHeroIntersect(Hero.GetDefaultRectangleInScreenCord())) Hero.Hearts.Decrease();
-        if (Hero.IsAttack && IsHeroIntersect(Hero.GetRectangleAttackInScreenCord()))
-            Hearts.Decrease(); // Герой нас дамажит
-
-
-        Move();
-
-        Hearts.Update();
-        Hearts.UpdatePosition(PositionInWorld);
-    }
-
-    public void Draw()
-    {
-        if (Hearts.IsDied) return;
-        Anims.Draw(PositionInWorld, Hearts.IsActiveShield);
-        Hearts.Draw();
-    }
-
-    private void Move()
-    {
-        var currentPoint = PathInWorldPoints[CurrentPointPathIndex].Item1;
-        var vector = currentPoint.ToVector2() - PositionInWorld;
-        var direction = vector == Vector2.Zero ? vector : Vector2.Normalize(vector);
-
-        PositionInWorld += Vector2.Distance(PositionInWorld, currentPoint.ToVector2()) < Speed
-            ? Vector2.Zero
-            : direction * Speed;
-        
-        if (WaitTime <= 0)
-        {
-            if (Vector2.Distance(PositionInWorld, currentPoint.ToVector2()) < Speed)
-            {
-                CurrentPointPathIndex++;
-                if (CurrentPointPathIndex >= PathInWorldPoints.Count) CurrentPointPathIndex = 0;
-                WaitTime = PathInWorldPoints[CurrentPointPathIndex].Item2;
-            }
-        }
-        else WaitTime -= Globals.TotalSeconds;
-
-        if (direction.X > 0 || (direction.X == 0 && direction.Y != 0)) Anims.Update("run_right");
-        else if (direction.X < 0) Anims.Update("run_left");
-        else Anims.Update("idle");
-    }
-
-    private bool IsHeroIntersect(RectangleF rectangleHero)
-    {
-        var demonPosition = Globals.Camera.WorldToScreen(PositionInWorld);
-        var rectangleDemon = new RectangleF(demonPosition.X, demonPosition.Y, 32 * Globals.Camera.Zoom,
-            36 * Globals.Camera.Zoom);
-
-        return rectangleDemon.Intersects(rectangleHero);
-    }
-
-    public void Init()
-    {
     }
 }
---
+++ Models/StupidMonster.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GetOut.Controllers;
+using GetOut.Program;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+
+namespace GetOut.Models;
+
+public abstract class StupidMonster : IEntityInterface
+{
+    public MapController MapController { get; set; }
+    public Hero Hero { get; set; }
+    public abstract Hearts Hearts { get; set; }
+    public abstract AnimationController Anims { get; init; }
+
+    protected Vector2 PositionInWorld { get; set; }
+    private float Speed { get; set; }
+    public abstract int Width { get; }
+    public abstract int Height { get; }
+
+    private List<Tuple<Point, int>> PathInWorldPoints { get; set; }
+    private int CurrentPointPathIndex { get; set; } = 0;
+    private float WaitTime { get; set; }
+
+    protected StupidMonster(Vector2 positionInWorld, List<Tuple<Point, int>> pathInWorldPoints, float speed)
+    {
+        PositionInWorld = positionInWorld;
+        PathInWorldPoints = pathInWorldPoints;
+        Speed = speed;
+        WaitTime = PathInWorldPoints.First().Item2;
+    }
+
+    public void Update()
+    {
+        if (Hearts.IsDied) return;
+
+        if (IsHeroIntersect(Hero.GetDefaultRectangleInScreenCord())) Hero.Hearts.Decrease();
+        if (Hero.IsAttack && IsHeroIntersect(Hero.GetRectangleAttackInScreenCord()))
+            Hearts.Decrease(); // Герой нас дамажит
+
+
+        Move();
+
+        Hearts.Update();
+        Hearts.UpdatePosition(PositionInWorld);
+    }
+
+    public void Draw()
+    {
+        if (Hearts.IsDied) return;
+        Anims.Draw(PositionInWorld, Hearts.IsActiveShield);
+        Hearts.Draw();
+    }
+
+    private void Move()
+    {
+        var currentPoint = PathInWorldPoints[CurrentPointPathIndex].Item1;
+        var vector = currentPoint.ToVector2() - PositionInWorld;
+        var direction = vector == Vector2.Zero ? vector : Vector2.Normalize(vector);
+
+        PositionInWorld += Vector2.Distance(PositionInWorld, currentPoint.ToVector2()) < Speed
+            ? Vector2.Zero
+            : direction * Speed;
+
+        if (WaitTime <= 0)
+        {
+            if (Vector2.Distance(PositionInWorld, currentPoint.ToVector2()) < Speed)
+            {
+                CurrentPointPathIndex++;
+                if (CurrentPointPathIndex >= PathInWorldPoints.Count) CurrentPointPathIndex = 0;
+                WaitTime = PathInWorldPoints[CurrentPointPathIndex].Item2;
+            }
+        }
+        else WaitTime -= Globals.TotalSeconds;
+
+        if (direction.X > 0 || (direction.X == 0 && direction.Y != 0)) Anims.Update("run_right");
+        else if (direction.X < 0) Anims.Update("run_left");
+        else Anims.Update("idle");
+    }
+
+    private bool IsHeroIntersect(RectangleF rectangleHero)
+    {
+        var monsterPosition = Globals.Camera.WorldToScreen(PositionInWorld);
+        var rectangleDemon = new RectangleF(monsterPosition.X, monsterPosition.Y, 32 * Globals.Camera.Zoom,
+            36 * Globals.Camera.Zoom);
+
+        return rectangleDemon.Intersects(rectangleHero);
+    }
+
+    public void Init()
+    {
+    }
+}
