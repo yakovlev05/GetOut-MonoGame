adf16892de9985a83d195fa258757ee9fa88cc09
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-06-03 22:55:33 +0500

    add abstract class SmartMonster (update PumpkinDude, Chort)

--- Models/PumpkinDude.cs
+++ Models/PumpkinDude.cs
@@ -1,37 +1,21 @@
-﻿using System;
-using System.Linq;
-using GetOut.Algorithms.BFS;
-using GetOut.Controllers;
+﻿using GetOut.Controllers;
 using GetOut.Program;
 using GetOut.View;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGame.Extended;
 
 namespace GetOut.Models;
 
-public class PumpkinDude : IEntityInterface
+public class PumpkinDude : SmartMonster
 {
-    public Hero Hero { get; set; }
-    public MapController MapController { get; set; }
+    protected sealed override Hearts Hearts { get; set; }
+    public sealed override AnimationController Anims { get; init; } = new();
+    protected override int Width => 16;
+    protected override int Height => 23;
 
-    private AnimationController Anims { get; init; } = new();
-    private Vector2 PositionInWorld { get; set; }
-    private Vector2 ActualDirection { get; set; }
-    private int Width => 16;
-    private int Height => 23;
-    private Hearts Hearts { get; set; }
-    private bool IsActive { get; set; } // Ищет ли игрока
-
-    private float Speed { get; set; }
-    private int StepsForActivate { get; set; } // Длина пути от монстра до героя, при котором монстр ничинает охоту
-
-    public PumpkinDude(Vector2 positionInWorld, float speed = 1, int stepsForActivate = 50, int heartsCount = 3)
+    public PumpkinDude(Vector2 positionInWorld, float speed = 1, int stepsForActivate = 20, int heartsCount = 3) :
+        base(positionInWorld, speed, stepsForActivate)
     {
-        PositionInWorld = positionInWorld;
-        Speed = speed;
-        StepsForActivate = stepsForActivate;
-
         Hearts = new Hearts(PositionInWorld, heartsCount, 0.35f, new Vector2(0, -6.5f), 3);
 
         var texture = Globals.Content.Load<Texture2D>("./Levels/assets/PumpkinDude");
@@ -40,95 +24,5 @@ public class PumpkinDude : IEntityInterface
         Anims.AddAnimation("run_right", new Animation(texture, 4, 2, 0.1f, 2, 4, offsetPosition: new Vector2(0, -7f)));
         Anims.AddAnimation("run_left",
             new Animation(texture, 4, 2, 0.1f, 2, 4, true, offsetPosition: new Vector2(0, -7f)));
-    }
-
-    public void Update()
-    {
-        if (Hearts.IsDied) return; // Монстр уже погиб
-
-        if (!IsActive)
-        {
-            ShouldFollowHero(); // Проверяем активность монстра (должен ли он идти за героем)
-            Anims.Update("idle");
-            return;
-        }
-
-
-        GoToHero();
-        if (Math.Abs(ActualDirection.X) > Math.Abs(ActualDirection.Y))
-        {
-            Anims.Update(ActualDirection.X > 0 ? "run_right" : "run_left");
-        }
-        else
-        {
-            Anims.Update(ActualDirection.Y != 0 ? "run_right" : "idle");
-        }
-
-        if (IsHeroIntersect(Hero.GetDefaultRectangleInScreenCord())) Hero.Hearts.Decrease(); // Дамажим героя
-        if (Hero.IsAttack && IsHeroIntersect(Hero.GetRectangleAttackInScreenCord())) Hearts.Decrease();
-
-        Hearts.Update();
-        Hearts.UpdatePosition(PositionInWorld);
-    }
-
-    public void Draw()
-    {
-        if (Hearts.IsDied) return;
-
-        Anims.Draw(PositionInWorld, Hearts.IsActiveShield);
-        Hearts.Draw();
-    }
-
-    public void Init()
-    {
-    }
-
-    private void GoToHero()
-    {
-        // Получаем коодинаты героя и переводим в координаты по тайлам с учётом погрешностей
-        var hero = Globals.Camera.ScreenToWorld(Hero.StartPosition);
-        var heroPosition = new Point((int)Math.Round(hero.X / 16.0 + 0.5),
-            (int)Math.Ceiling((hero.Y + 16 * 2) / 16.0) - 1);
-
-        var dudePosition =
-            new Point((int)Math.Round(PositionInWorld.X / 16), (int)Math.Floor((PositionInWorld.Y / 16)));
-
-        // Поиск в ширину, старт - позиция монстра, финиш - позиция героя
-        var path = Bfs.FindPath(MapController.Map, dudePosition, heroPosition);
-
-        if (path.Count < 2)
-        {
-            ActualDirection = Vector2.Zero;
-            return;
-        } // Поиск возващае путь вместе с начальной точкой
-
-        var nextStepPoint = path.Skip(1).First();
-
-
-        var target = new Vector2(nextStepPoint.X * 16, nextStepPoint.Y * 16);
-        var direction = Vector2.Normalize(target - PositionInWorld) * Speed;
-        PositionInWorld += direction * Speed;
-
-        ActualDirection = direction; // Обновляем позици для обновления анимации
-    }
-
-    private bool IsHeroIntersect(RectangleF rectangleHero)
-    {
-        var dudePosition = Globals.Camera.WorldToScreen(PositionInWorld);
-        var rectangleDude = new RectangleF(dudePosition.X, dudePosition.Y, Width * Globals.Camera.Zoom,
-            Height * Globals.Camera.Zoom);
-        return rectangleDude.Intersects(rectangleHero);
-    }
-
-    private void ShouldFollowHero()
-    {
-        var hero = Globals.Camera.ScreenToWorld(Hero.StartPosition);
-        var heroPosition = new Point((int)Math.Round(hero.X / 16.0 + 0.5),
-            (int)Math.Ceiling((hero.Y + 16 * 2) / 16.0) - 1);
-        var dudePosition =
-            new Point((int)Math.Round(PositionInWorld.X / 16), (int)Math.Floor((PositionInWorld.Y / 16)));
-
-        var path = Bfs.FindPath(MapController.Map, dudePosition, heroPosition);
-        IsActive = path.Count <= StepsForActivate;
     }
 }
---
+++ Models/SmartMonster.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using GetOut.Algorithms.BFS;
+using GetOut.Controllers;
+using GetOut.Program;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+
+namespace GetOut.Models;
+
+public abstract class SmartMonster : IEntityInterface
+{
+    public Hero Hero { get; set; }
+    public MapController MapController { get; set; }
+    protected abstract Hearts Hearts { get; set; }
+    public abstract AnimationController Anims { get; init; }
+
+    protected Vector2 PositionInWorld { get; set; }
+    private Vector2 ActualDirection { get; set; }
+    private float Speed { get; set; }
+    private bool IsActive { get; set; } // Ищет ли игрока
+    private int StepsForActivate { get; set; } // Длина пути от монстра до героя, при котором монстр ничинает охоту
+    protected abstract int Width { get; }
+    protected abstract int Height { get; }
+
+    protected SmartMonster(Vector2 positionInWorld, float speed, int stepsForActivate)
+    {
+        PositionInWorld = positionInWorld;
+        Speed = speed;
+        StepsForActivate = stepsForActivate;
+    }
+
+    public void Update()
+    {
+        if (Hearts.IsDied) return; // Монстр уже погиб
+
+        if (!IsActive)
+        {
+            ShouldFollowHero(); // Проверяем активность монстра (должен ли он идти за героем)
+            Anims.Update("idle");
+            return;
+        }
+
+
+        GoToHero();
+        if (Math.Abs(ActualDirection.X) > Math.Abs(ActualDirection.Y))
+        {
+            Anims.Update(ActualDirection.X > 0 ? "run_right" : "run_left");
+        }
+        else
+        {
+            Anims.Update(ActualDirection.Y != 0 ? "run_right" : "idle");
+        }
+
+        if (IsHeroIntersect(Hero.GetDefaultRectangleInScreenCord())) Hero.Hearts.Decrease(); // Дамажим героя
+        if (Hero.IsAttack && IsHeroIntersect(Hero.GetRectangleAttackInScreenCord())) Hearts.Decrease();
+
+        Hearts.Update();
+        Hearts.UpdatePosition(PositionInWorld);
+    }
+
+    public void Draw()
+    {
+        if (Hearts.IsDied) return;
+
+        Anims.Draw(PositionInWorld, Hearts.IsActiveShield);
+        Hearts.Draw();
+    }
+
+    public void Init()
+    {
+    }
+
+    private void GoToHero()
+    {
+        // Получаем коодинаты героя и переводим в координаты по тайлам с учётом погрешностей
+        var hero = Globals.Camera.ScreenToWorld(Hero.StartPosition);
+        var heroPosition = new Point((int)Math.Round(hero.X / 16.0 + 0.5),
+            (int)Math.Ceiling((hero.Y + 16 * 2) / 16.0) - 1);
+
+        var monsterPosition =
+            new Point((int)Math.Round(PositionInWorld.X / 16), (int)Math.Floor((PositionInWorld.Y / 16)));
+
+        // Поиск в ширину, старт - позиция монстра, финиш - позиция героя
+        var path = Bfs.FindPath(MapController.Map, monsterPosition, heroPosition);
+
+        if (path.Count < 2)
+        {
+            ActualDirection = Vector2.Zero;
+            return;
+        } // Поиск возващае путь вместе с начальной точкой
+
+        var nextStepPoint = path.Skip(1).First();
+
+
+        var target = new Vector2(nextStepPoint.X * 16, nextStepPoint.Y * 16);
+        var direction = Vector2.Normalize(target - PositionInWorld) * Speed;
+        PositionInWorld += direction * Speed;
+
+        ActualDirection = direction; // Обновляем позици для обновления анимации
+    }
+
+    private bool IsHeroIntersect(RectangleF rectangleHero)
+    {
+        var dudePosition = Globals.Camera.WorldToScreen(PositionInWorld);
+        var rectangleDude = new RectangleF(dudePosition.X, dudePosition.Y, Width * Globals.Camera.Zoom,
+            Height * Globals.Camera.Zoom);
+        return rectangleDude.Intersects(rectangleHero);
+    }
+
+    private void ShouldFollowHero()
+    {
+        var hero = Globals.Camera.ScreenToWorld(Hero.StartPosition);
+        var heroPosition = new Point((int)Math.Round(hero.X / 16.0 + 0.5),
+            (int)Math.Ceiling((hero.Y + 16 * 2) / 16.0) - 1);
+        var monsterPosition =
+            new Point((int)Math.Round(PositionInWorld.X / 16), (int)Math.Floor((PositionInWorld.Y / 16)));
+
+        var path = Bfs.FindPath(MapController.Map, monsterPosition, heroPosition);
+        IsActive = path.Count <= StepsForActivate;
+    }
+}
