6f5515f1c26c421e5530ce64496f374288f323c7
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-06-05 23:09:26 +0500

    remove unused content files

--- Content/level1/atlas_floor-16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="atlas_floor-16x16" tilewidth="16" tileheight="16" tilecount="49" columns="7">
- <image source="./atlas_floor-16x16.png" width="112" height="112"/>
-</tileset>
--- Content/level1/atlas_walls_low-16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="atlas_walls_low-16x16" tilewidth="16" tileheight="16" tilecount="48" columns="12">
- <image source="./atlas_walls_low-16x16.png" width="192" height="64"/>
-</tileset>
